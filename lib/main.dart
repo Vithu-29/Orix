c4ce8b0d44ee7e35b09449a8b56b8cb6cb37a579
Vithushan Kanesamoorthy <vithuvithush44@gmail.com>
2025-12-03 20:44:14 +0530

    Refactor:Initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:ecommerce_flutter/main_app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   //Todo: Add Widgets Binding
   //Todo: Init Local Storage
   //Todo: Await Native splash
-  //Todo: Initialize Firebase
+  //Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //Todo: Initialize Authentication
 
   runApp(const MainApp());
